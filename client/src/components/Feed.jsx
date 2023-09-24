d25ebc9f9d2b0ab0aeba44fe6116139379449a6a
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-24 21:05:12 +0100

    done with feed and post components

--- client/src/components/Feed.jsx
+++ client/src/components/Feed.jsx
@@ -1,12 +1,17 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import Post from "./Post";
 
 function Feed() {
-  return (
-    <Box bgcolor="skyblue" p={2} flex={4}>
-      Feed
-    </Box>
-  );
+  return <Box p={2} flex={4}>
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+  </Box>;
 }
 
-export default Feed
+export default Feed;
--- client/src/components/Rightbar.jsx
+++ client/src/components/Rightbar.jsx
@@ -1,15 +1,14 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 function Rightbar() {
   return (
-    <Box
-      bgcolor="red"
-      p={2}
-      flex={2}
-      sx={{ display: { xs: "none", sm: "block" } }}
-    >
-      RightBar
+    <Box p={2} flex={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      <Box position="fixed">
+        <Typography variant='h6' fontWeight={100}>
+          Online Friends
+        </Typography>
+      </Box>
     </Box>
   );
 }
